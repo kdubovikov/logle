9196ce5c528a07e0ced68f4a3024ef11b776a386
Kirill Dubovikov <kernelmod3@gmail.com>
2014-10-30 21:20:49 +0300

    UPDATE: added linear filtering

--- render/Texture.cpp
+++ render/Texture.cpp
@@ -15,9 +15,12 @@ bool Texture::load(const std::string& imagePath) {
 }
 
 void Texture::prepareTexture() {
-    glBindTexture(GL_TEXTURE_2D, textureId);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    // When MAGnifying the image (no bigger mipmap available), use LINEAR filtering
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // When MINifying the image, use a LINEAR blend of two mipmaps, each filtered LINEARLY too
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    // Generate mipmaps, by the way.
+    glGenerateMipmap(GL_TEXTURE_2D);
 }
 
 GLuint Texture::getTextureId() {
